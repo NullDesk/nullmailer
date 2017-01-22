b9f3f3b1519ac0fa1cf1a35c2598f4aff3292f8d
Stephen Redd <redds@reddnet.net>
2017-01-22 02:01:06 -0500

    initial ci test

--- build.ps1
+++ build.ps1
@@ -1,63 +1,106 @@
-param([string]$config)
-$solutionVer = $null
-$rootDir = Get-Location
-$srcDir = Get-ChildItem ./src
-$testDir = Get-ChildItem ./test
-if(!$config){
-    $config = "Debug"
-}
-Write-Output "Building $config configuration"
-Write-Output "--------------------------------------"
-#loop through src folders and restore & build
-foreach ($folder in $srcDir) {
-    $p = Join-Path -Path $folder.FullName -ChildPath 'project.json';
-    
-    # only restore projects -> folders with a project.json file 
-    if (Test-Path $p -PathType Leaf) {
-        Set-Location $folder.FullName
-        dotnet restore
-        dotnet build --configuration $config
-    }
-}
-#loop through test folders and restore & build
-foreach ($folder in $testDir) {
-    $p = Join-Path -Path $folder.FullName -ChildPath 'project.json';
-    
-    # only restore projects -> folders with a project.json file 
-    if (Test-Path $p -PathType Leaf) {
-        Set-Location $folder.FullName
-        dotnet restore
-        dotnet build --configuration $config
+<#  
+.SYNOPSIS
+    You can add this to you build script to ensure that psbuild is available before calling
+    Invoke-MSBuild. If psbuild is not available locally it will be downloaded automatically.
+#>
+function EnsurePsbuildInstalled{  
+    [cmdletbinding()]
+    param(
+        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
+    )
+    process{
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
+            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
+        }
+        else{
+            'psbuild already loaded, skipping download' | Write-Verbose
+        }
+
+        # make sure it's loaded and throw if not
+        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
+            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
+        }
     }
 }
 
-# loop through projects under ./src
+# Taken from psake https://github.com/psake/psake
+
+<#  
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+
+if(Test-Path .\dist) {
+    Remove-Item .\dist -Force -Recurse 
+}
+
+EnsurePsbuildInstalled
+
+exec { & dotnet restore }
+
+$repoTag = @{ $true = $env:APPVEYOR_REPO_TAG_NAME; $false = $NULL }[$env:APPVEYOR_REPO_TAG_NAME -ne $NULL];
+#only care about tags that look like version numbers 
+$isBeta = -not $repoTag -match "^[V|v]?\d+\.\d+\.\d+"
+
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "beta-{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+$srcDir = Get-ChildItem ./src
+
+[object[]]$projectFolders = $NULL
+[object[]]$testFolders = $NULL
+
+if(!$config){
+    $config = "Release"
+}
+
+# loop through projects and collect src and test project paths
 foreach ($folder in $srcDir) {
     $p = Join-Path -Path $folder.FullName -ChildPath 'project.json';
-    
     # only src project folders -> folders with a project.json file 
     if (Test-Path $p -PathType Leaf) {
-        Set-Location $folder.FullName
-        
+        $projectFolders += $folder.FullName
         # find the test project, if one exists, and run each
         $testFolderPath = "..\..\test\" + $folder.Name + ".Tests"
         if (Test-Path $testFolderPath -PathType Container){
             $x = Join-Path -Path $testFolderPath -ChildPath 'xunit.runner.json';
             if (Test-Path $x -PathType Leaf) {
-                Write-Output ""
-                Write-Output "Next project: $testFolderPath"
-                Set-Location $testFolderPath
-                
-                dotnet test --configuration $config -trait "TestType=Unit"
-                
-                Set-Location $folder.FullName
+                $testFolders += $testFolderPath    
             }
-        }
-        else{
-            Write-Output ""
-            Write-Output "Test project not found for source project '$folder'"
         }
     }
 }
-Set-Location $rootDir
-Write-Output Done
+
+# run tests first
+foreach($testFolder in $testFolders){
+    exec { & dotnet test $testFolder --configuration $config -trait "TestType=Unit" }
+}
+
+# package src projects
+foreach($srcFolder in $projectFolders){
+    if($isBeta){
+        exec { & dotnet pack $srcFolder -c $config -o .\dist --version-suffix=$revision }
+    }
+    else {
+        exec { & dotnet pack $srcFolder -c $config -o .\dist }
+
+    }
+}
