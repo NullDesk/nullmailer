bc38f987c7e4a5bf3f939e4ceb94bc762fd639f7
Stephen Redd <redds@reddnet.net>
2018-11-16 03:21:30 -0800

    Fix EF migration tooling

---
+++ src/NullDesk.Extensions.Mailer.History.EntityFramework.SqlServer/SqlHistoryContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NullDesk.Extensions.Mailer.History.EntityFramework.SqlServer
+{
+    /// <summary>
+    ///     Class SqlHistoryContextFactory.
+    /// </summary>
+    public class SqlHistoryContextFactory : IDesignTimeDbContextFactory<SqlHistoryContext>
+    {
+        /// <summary>
+        ///     Creates a new instance of the  context.
+        /// </summary>
+        /// <param name="args">Arguments provided by the design-time service.</param>
+        /// <returns>An instance of <typeparamref name="TContext" />.</returns>
+        /// <remarks>Used by EF CLI tooling</remarks>
+        public SqlHistoryContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<HistoryContext>();
+
+            builder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=MailerCliHistory;Trusted_Connection=True;");
+            return new SqlHistoryContext(builder.Options);
+        }
+    }
+}
---
+++ test/NullDesk.Extensions.Mailer.History.EntityFramework.SqlServer.Tests/Infrastructure/TestSqlHistoryContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NullDesk.Extensions.Mailer.History.EntityFramework.SqlServer.Tests.Infrastructure
+{
+    /// <summary>
+    ///     Class TestSqlHistoryContextFactory.
+    /// </summary>
+    /// <remarks>
+    ///     The connection details aren't that important, this a work around to allow ef tooling to function correctly from a
+    ///     class library.
+    /// </remarks>
+    public class TestSqlHistoryContextFactory : IDesignTimeDbContextFactory<TestSqlHistoryContext>
+    {
+        /// <summary>
+        ///     Creates a new instance of the  context.
+        /// </summary>
+        /// <param name="args">Arguments provided by the design-time service.</param>
+        /// <returns>An instance of <typeparamref name="TContext" />.</returns>
+        /// <remarks>Used by EF CLI tooling</remarks>
+        public TestSqlHistoryContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<HistoryContext>();
+
+            builder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=NullMailerHistory;Trusted_Connection=True;");
+            return new TestSqlHistoryContext(builder.Options);
+        }
+    }
+}
